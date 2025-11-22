0f5ab3ba5e275674ea2d8077abb5f3f9ed00a959
Jaden <jegedejones@gmail.com>
2025-11-21 21:47:26 -0500

    Exercise 2

---
+++ app.js
@@ -0,0 +1,13 @@
+document.getElementById("searchBtn").addEventListener("click", function () {
+
+    fetch("superheroes.php")
+        .then(response => response.text())
+        .then(data => {
+            alert(data);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+
+});
+
